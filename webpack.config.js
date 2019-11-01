6d9e4c63b4942beaa36208a246af4e69d85c3aad
DmitriyShvanyk <soledar1984@gmail.com>
2019-11-01 19:02:28 +0200

    Add pages css index, project, analytics

--- webpack.config.js
+++ webpack.config.js
@@ -13,9 +13,9 @@ const isDev = process.env.NODE_ENV === 'development'
 
 module.exports = {
   entry: {    
-    index: [`./src/js/index.js`, `./src/index.css`],
-    project: [`./src/js/project.js`, `./src/project.css`],
-    analytics: [`./src/js/analytics.js`, `./src/analytics.css`]
+    index: [`./src/js/index.js`, `./src/pages/index.css`],
+    project: [`./src/js/project.js`, `./src/pages/project.css`],
+    analytics: [`./src/js/analytics.js`, `./src/pages/analytics.css`]
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
