01a9ff201de50d77e9ba1f34e9a828d76d45dc6a
DmitriyShvanyk <soledar1984@gmail.com>
2019-11-07 20:29:11 +0200

    when you re-request, we clean news cards

--- src/js/index.js
+++ src/js/index.js
@@ -9,6 +9,99 @@ const apiNews = new ApiNews('https://newsapi.org/v2/everything?', {
 });
 
 
+
+
+
+const resultsContainer = document.querySelector('.results__container');
+//const resultsInner = document.querySelector('.results__inner');
+//resultsInner.style.display = 'none';
+
+
+class Results{
+  constructor(container){
+    this.container = container;
+  }
+  showNotFound(){
+    let containerFragment = document.createDocumentFragment();
+
+    let notFoundElement = document.createElement('div');
+    notFoundElement.classList.add('not-found');
+  
+    let notFoundElementIcon = document.createElement('img');
+    notFoundElementIcon.classList.add('not-found__icon');
+    notFoundElementIcon.src = './images/i-not-found.svg';
+    notFoundElementIcon.alt = 'Ничего не найдено';
+  
+    let notFoundElementTitle = document.createElement('h2');
+    notFoundElementTitle.classList.add('not-found__title');
+    notFoundElementTitle.textContent = 'Ничего не найдено';
+  
+    let notFoundElementText = document.createElement('p');
+    notFoundElementText.classList.add('not-found__text');
+    notFoundElementText.textContent = 'К сожалению по вашему запросу ничего не найдено.';
+  
+    notFoundElement.appendChild(notFoundElementIcon);
+    notFoundElement.appendChild(notFoundElementTitle);
+    notFoundElement.appendChild(notFoundElementText);
+
+    containerFragment.appendChild(notFoundElement);
+
+    this.container.appendChild(notFoundElement);
+  
+    return notFoundElement;
+  }
+
+  showPreloader(){
+    let containerFragment = document.createDocumentFragment();
+
+    let preloader = document.createElement('div');
+    preloader.classList.add('preloader', 'preloader--news');
+
+    let preloaderIcon = document.createElement('div');
+    preloaderIcon.classList.add('preloader__icon');
+
+    let preloaderText = document.createElement('div');
+    preloaderText.classList.add('preloader__text');
+    preloaderText.textContent = 'Идет поиск новостей...';
+
+    preloader.appendChild(preloaderIcon);
+    preloader.appendChild(preloaderText);
+
+    containerFragment.appendChild(preloader);
+
+    this.container.appendChild(preloader);
+
+    return preloader;
+  }
+
+  removeNotFound(target){    
+    this.container.removeChild(target); 
+  }
+
+  /*removePreloader(){
+    if(this.showNotFound()){
+      this.container.removeChild(this.container.firstChild);
+    }  
+  }*/
+
+  clearCardsNews(){
+    while(newsContainer.firstChild){
+      newsContainer.removeChild(newsContainer.firstChild);
+    }
+  }
+ 
+}
+
+const results = new Results(resultsContainer);
+
+
+
+
+
+
+
+
+
 // Validation
 import Validate from "./modules/validate.js"
 
@@ -53,12 +146,23 @@ formSearchValidate.addEventListener('submit', (event) => {
     const dateFrom = new Date(dateTime - date6Days).toISOString();
     const dateTo = date.toISOString();
 
+    // при повторном запросе делаем очистку карточек с новостями
+    results.clearCardsNews()
+
     apiNews.getInitialNewsCards(text, dateFrom, dateTo)
-      .then((cards) => {        
+      .then((cards) => {       
+             
 
-        new CardListNews(newsContainer, cards);
+        if (!cards.ok) {
+          //results.showPreloader();
+        }     
 
-      }).catch(function (err) {
+        new CardListNews(newsContainer, cards);       
+
+
+      }).catch(function (err) {    
+        
+        //results.showNotFound();
 
         return Promise.reject(`Ошибка: ${err.status}`);
 
